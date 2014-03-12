4d7c55b8555df4445838dbb99e901c55ee752174
shigeu1228 <shigeu1228@gmail.com>
2014-03-13 01:55:54 +0900

    Add test

--- test/test.js
+++ test/test.js
@@ -2,11 +2,20 @@ var expect = require('expect.js');
 
 var service = require('../services/service.js');
 
+/**
+ * テストコードを記述していきます
+ */
 describe('#test', function() {
+
+  /**
+   * add関数のテストをします。　
+   * 2つの引数を渡し、コールバックで足した値を受け取れるかのテストです。
+   */
   it('add test', function(done) {
     service.add(2, 3, function(result) {
       expect(result).to.be(5);
       done();
     });
-  }); 
+  });
+
 }); 
